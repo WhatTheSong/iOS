21b69a6248da308aaf6d928cf29dba9344ccefdb
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-01-10 13:26:52 +0900

    Project Init

---
+++ WhatTheSong/WhatTheSong/WhatTheSongApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatTheSongApp.swift
+//  WhatTheSong
+//
+//  Created by 강민규 on 2023/01/10.
+//
+
+import SwiftUI
+
+@main
+struct WhatTheSongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
