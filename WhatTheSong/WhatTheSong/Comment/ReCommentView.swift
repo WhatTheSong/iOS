4e45a30260fbcfb54f79065f55dd9de29ff82cbf
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-01-14 03:05:26 +0900

    [Feat #3] ReCommentView 생성

---
+++ WhatTheSong/WhatTheSong/Comment/ReCommentView.swift
@@ -0,0 +1,20 @@
+//
+//  ReCommentView.swift
+//  WhatTheSong
+//
+//  Created by 강민규 on 2023/01/14.
+//
+
+import SwiftUI
+
+struct ReCommentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ReCommentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReCommentView()
+    }
+}
