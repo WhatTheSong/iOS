168dd0e8333ff0503810d1fa5711a052a1df096b
geun0196 <geun0196@naver.com>
2023-01-17 18:57:00 +0900

    FEAT: Creat RecordCell

--- WhatTheSong/WhatTheSong/Common/RecordCell/RecordCell.swift
+++ WhatTheSong/WhatTheSong/Common/RecordCell/RecordCell.swift
@@ -8,50 +8,83 @@
 import SwiftUI
 
 struct RecordCell: View {
-    @State var silderValue: Double = 0.0
-
+    @EnvironmentObject var audioManager: AudioManager
+    var meditationVM: MeditationViewModel
+    var isPreview : Bool = false
+    @State private var isEditing: Bool = false
+    @State private var silderValue: Double = 0.0
+    
+    let timer = Timer
+        .publish(every: 1, on: .main, in: .common)
+        .autoconnect()
+    
     var body: some View {
         VStack(){
             HStack(){
-                Image(systemName: "photo")
+                Image("vynil")
+                    .resizable()
+                    .frame(width: 100,height: 100)
                 
                 Spacer()
+                    .frame(width: 20)
                 
                 ExplainView()
             }
             .padding(20)
             
             HStack{
+                let player = audioManager.player
+                
                 // MARK: Play Button
-                PlayButton(systemName: "play.fill", action: {})
+                PlayButton(systemName: player?.isPlaying ?? false ? "pause.circle" : "play.fill", action: {
+                    if ((player?.isPlaying) != nil) {
+                        audioManager.playPause()
+                    } else {
+                        audioManager.startPlayer(track: meditationVM.meditation.track, isPreview: isPreview)
+                    }
+                })
                 .padding(.trailing, 15)
                 
                 // MARK: Music Slider
                 VStack(spacing: 5){
-                    Slider(value: $silderValue, in: 0...60)
-                        .accentColor(.black)
+                    Slider(value: $silderValue, in: 0...(player?.duration ?? 0.0)){ editing in
+                        
+                        print("editing", editing)
+                        isEditing = editing
+                        
+                        if !editing {
+                            player?.currentTime = silderValue
+                        }
+                    }
+                    .accentColor(.black)
                     
                     HStack{
-                        Text("0:00")
+                        Text(DateComponentsFormatter.positional.string(from: player?.currentTime ?? 0.0) ?? "0:00")
                         
                         Spacer()
                         
-                        Text("3:00")
+                        Text((DateComponentsFormatter.positional.string(from: meditationVM.meditation.duration - (player?.currentTime ?? 0.0)) ?? ""))
                     }
                     .font(.caption)
                     .foregroundColor(.gray)
-                    
                 }
             }
         }
         //.border(Color.gray.opacity(1), width: 1)
         .padding(20)
-
+        .onReceive(timer) { _ in
+            guard let player = audioManager.player, !isEditing else { return }
+            silderValue = player.currentTime
+        }
+        
     }
 }
 
 struct RecordCell_Previews: PreviewProvider {
+    static let meditationVM = MeditationViewModel(meditation: Meditation.data)
+    
     static var previews: some View {
-        RecordCell()
+        RecordCell(meditationVM: meditationVM, isPreview: true)
+            .environmentObject(AudioManager())
     }
 }
--- WhatTheSong/WhatTheSong/WhatTheSongApp.swift
+++ WhatTheSong/WhatTheSong/WhatTheSongApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct WhatTheSongApp: App {
+    @StateObject var audioManager = AudioManager()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(audioManager)
         }
     }
 }
