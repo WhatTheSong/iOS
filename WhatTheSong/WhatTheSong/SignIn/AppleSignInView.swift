c5cf2bdb04806f31299e2b7fbec8a5c7c447124b
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-01-26 23:47:16 +0900

    [Feat #16] 애플 로그인 버튼 추가

--- WhatTheSong/WhatTheSong/SignIn/AppleSignInView.swift
+++ WhatTheSong/WhatTheSong/SignIn/AppleSignInView.swift
@@ -6,10 +6,43 @@
 //
 
 import SwiftUI
+import AuthenticationServices
+
+struct SignInWithAppleSwiftUIButton: View {
+    @Environment(\.colorScheme) var colorScheme
+
+    var body: some View {
+      if colorScheme.self == .dark {
+          SignInButton(SignInWithAppleButton.Style.whiteOutline)
+      }
+      else {
+          SignInButton(SignInWithAppleButton.Style.black)
+      }
+    }
+
+    func SignInButton(_ type: SignInWithAppleButton.Style) -> some View{
+        return SignInWithAppleButton(.signIn) { request in
+            request.requestedScopes = [.fullName, .email]
+        } onCompletion: { result in
+            switch result {
+            case .success(let authResults):
+                print("Authorisation successful \(authResults)")
+            case .failure(let error):
+                print("Authorisation failed: \(error.localizedDescription)")
+            }
+        }
+        .frame(width: 280, height: 60, alignment: .center)
+        .signInWithAppleButtonStyle(type)
+    }
+}
 
 struct AppleSignInView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            SignInWithAppleSwiftUIButton()
+        }
     }
 }
 
