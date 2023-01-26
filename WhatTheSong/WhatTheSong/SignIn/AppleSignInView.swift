a70928aef3471fbb9a02cfbe53694f6e82bc9ebe
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-01-26 23:21:03 +0900

    [Feat #16] 애플 로그인 파일 초기화

---
+++ WhatTheSong/WhatTheSong/SignIn/AppleSignInView.swift
@@ -0,0 +1,20 @@
+//
+//  AppleSignInView.swift
+//  WhatTheSong
+//
+//  Created by 강민규 on 2023/01/26.
+//
+
+import SwiftUI
+
+struct AppleSignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AppleSignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppleSignInView()
+    }
+}
