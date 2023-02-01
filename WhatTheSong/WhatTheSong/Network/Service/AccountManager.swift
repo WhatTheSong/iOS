8e38f9c3a433f7f6d06162a8ade113aa5353566e
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-02-01 19:46:34 +0900

    [Feat #20] 네트워킹 폴더링, POST 함수

---
+++ WhatTheSong/WhatTheSong/Network/Service/AccountManager.swift
@@ -0,0 +1,26 @@
+//
+//  AccountManager.swift
+//  WhatTheSong
+//
+//  Created by 강민규 on 2023/02/01.
+//
+
+import Foundation
+
+import Alamofire
+
+class AccountManager {
+    func postAccount(account: AccountModel, platform: SignInPlatform) {
+        let url: String
+        switch platform {
+        case .Apple:
+            url = URLs.testURL + URLs.appleUserURL
+        case .Kakao:
+            url = URLs.testURL + URLs.kakaoUserURL
+        }
+        
+        APIManager.request(url: url, params: ParameterManager.makeAccountParameter(account: account))
+        
+    }
+}
+
