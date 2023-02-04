ae44a9a1c6a5f568e4100b3fe9be1db7bc65584e
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-02-04 17:40:36 +0900

    [Feat #27] 추천 답변 페이지

---
+++ WhatTheSong/WhatTheSong/Presenter/Recommend/RecommendDetailView.swift
@@ -0,0 +1,44 @@
+//
+//  RecommendDetailView.swift
+//  WhatTheSong
+//
+//  Created by 강민규 on 2023/02/04.
+//
+
+import SwiftUI
+
+struct RecommendDetailView: View {
+    var body: some View {
+        VStack {
+            NavigationStack {
+                // 게시글 사진
+                Text("사진")
+                // 좋아요 숫자
+                Text("좋아요 숫자")
+                // 게시글 내용
+                Group {
+                    VStack {
+                        Spacer()
+                        Text("내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용 내용")
+                            .background(.red)
+                    }
+                }
+                .padding(.bottom, UIScreen.screenHeight - 375)
+                .navigationTitle("아 그 노래 뭐더라 ")
+                .navigationBarTitleDisplayMode(.inline)
+                .background(.blue)
+                
+            }
+            .overlay(alignment: .bottom) {
+                CommentView()
+                    .offset(x:0,y:280)
+            }
+        }
+    }
+}
+
+struct RecommendDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecommendDetailView()
+    }
+}
---
+++ WhatTheSong/WhatTheSong/Presenter/Recommend/RecommendView.swift
@@ -0,0 +1,20 @@
+//
+//  RecommendView.swift
+//  WhatTheSong
+//
+//  Created by 강민규 on 2023/02/04.
+//
+
+import SwiftUI
+
+struct RecommendView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecommendView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecommendView()
+    }
+}
